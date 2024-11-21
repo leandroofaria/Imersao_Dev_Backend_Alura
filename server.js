576609c88d2c8e08ae107f01eb1f95703f63de86
leandroofaria <leandrogabrielfaria05@gmail.com>
2024-11-21 10:31:41 -0300

    Iniciando Projeto

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("Servidor Escutando...")
+})
+
+
+app.get("/api", (req,res) => {
+    res.status(200).send("Boas vindas à imersão!")
+})
